3185c1bab85f32e855d66e3d5ade6029815315d5
James-Livesey <jamesliveg@gmail.com>
2021-03-31 15:55:37 +0100

    Add framework essentials

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Adapt-UI
+# Adapt UI
 Our user interface design library and tools for making great apps.
+
+Licenced by the [LiveG Open-Source Licence](LICENCE.md).
---
+++ src/adaptui.js
@@ -0,0 +1,8 @@
+/*
+    LiveG App Runtime
+
+    Copyright (C) LiveG. All Rights Reserved.
+
+    https://liveg.tech
+    Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
+*/
