3f51ea3ccf8ad77dad6c2319e72f9e6fb97175ca
James-Livesey <jamesliveg@gmail.com>
2021-05-18 19:20:44 +0100

    Add l10n support

---
+++ src/l10n.js
@@ -0,0 +1,87 @@
+/*
+    Adapt UI
+
+    Copyright (C) LiveG. All Rights Reserved.
+
+    https://liveg.tech
+    Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
+*/
+
+import * as requests from "./requests.js";
+
+export class TranslationError extends error {
+    constructor(message) {
+        super(message);
+
+        this.name = this.constructor.name;
+    }
+}
+
+export class Locale {
+    constructor(localeCode, source, fallbackLocale = null) {
+        this.localeCode = localeCode;
+        this.source = source;
+        this.fallbackLocale = fallbackLocale;
+    }
+
+    static fromResource(url, fallbackLocale = null) {
+        return requests.json(url).then(function(data) {
+            return Promise.resolve(new this(data.localeCode, data.source, fallbackLocale));
+        });
+    }
+
+    format(data, options) {
+        if (typeof(data) == "number") {
+            return Number(data).toLocaleString(this.localeCode.replace(/_/g, "-"), options);
+        } else if (data instanceof Date) {
+            return data.toLocaleString(this.localeCode.replace(/_/g, "-"), options);
+        } else {
+            return data;
+        }
+    }
+
+    translate(string, args = {}, applyFormatting = true) {
+        if (applyFormatting) {
+            Object.keys(args).map((i) => args[i] = this.format(args[i]));
+        }
+
+        if (this.source[string] == undefined) {
+            if (this.fallbackLocale != null) {
+                return this.fallbackLocale.translate(string, args, applyFormatting);
+            }
+
+            throw new TranslationError(`Could not find translation for string \`${string}\``);
+        }
+
+        var translation = this.source[string];
+
+        // Rule-based translation (dealing with, for example, plural forms)
+        if (translation instanceof Object) {
+            for (var rule in translation) {
+                var replacedRule = rule;
+
+                for (var key in args) {
+                    replacedRule = String(replacedRule).split("{" + key + "}").join(args[key]);
+                }
+
+                try {
+                    if (eval(replacedRule)) {
+                        translation = translation[rule];
+
+                        break;
+                    }
+                } catch (e) {}
+            }
+
+            if (translation instanceof Object) {
+                var lastRule = Object.keys(translation)[Object.keys(translation) - 1];
+
+                translation = translation[lastRule];
+            }
+        }
+
+        for (var key in args) {
+            translation = String(translation).split("{" + key + "}").join(args[key]); // Replace arguments globally without RegEx
+        }
+    }
+}
---
+++ src/requests.js
@@ -0,0 +1,44 @@
+/*
+    Adapt UI
+
+    Copyright (C) LiveG. All Rights Reserved.
+
+    https://liveg.tech
+    Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
+*/
+
+export function send(method, url) {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+
+        xhr.onload = function() {
+            resolve({
+                status: xhr.status,
+                response: xhr.response
+            });
+        };
+
+        xhr.open(method, url);
+        xhr.send();
+    });
+}
+
+export function get(url) {
+    return send("GET", url);
+}
+
+export function json(url) {
+    return new Promise(function(resolve, reject) {
+        return send("GET", url).then(function(response) {
+            try {
+                resolve(JSON.parse(response));
+            } catch (e) {
+                if (e instanceof SyntaxError) {
+                    reject("Syntax error while parsing JSON");
+                } else {
+                    throw e;
+                }
+            }
+        });
+    });
+}
