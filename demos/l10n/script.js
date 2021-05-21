4f5efc6c1660612b9fca5bdfa516c02046f5111b
James-Livesey <jamesliveg@gmail.com>
2021-05-21 08:28:12 +0100

    Add Russian translation demo bc why not lol

--- demos/l10n/script.js
+++ demos/l10n/script.js
@@ -5,7 +5,8 @@ function applyL10n(localeCode = $g.l10n.getSystemLocaleCode()) {
         "en_GB": "locales/en_GB.json",
         "fr_FR": "locales/fr_FR.json",
         "ar_AE": "locales/ar_AE.json",
-        "zh_CN": "locales/zh_CN.json"
+        "zh_CN": "locales/zh_CN.json",
+        "ru_RU": "locales/ru_RU.json"
     }, "en_GB", {}, localeCode).then(function(locale) {
         window._ = function() {
             return locale.translate(...arguments);
